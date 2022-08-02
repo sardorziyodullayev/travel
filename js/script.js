52f5e7f31681392cc3fb5e9177876a81fe60725e
sardorziyodullayev <sardorziyodullayev2004@gmail.com>
2022-08-02 19:18:02 +0500

    Initialy commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var elText = document.querySelector(".text");
+
+var result = Number(prompt("Qancha pulingiz bor?").trim());
+
+var phonePrice = 2000;
+
+if (isNaN(result)) {
+  elText.textContent = "Iltimos, son kiriting!";
+} else {
+  if (result >= phonePrice) {
+    elText.textContent = "Tabriklaymiz bilet sotib olishga pulingiz yetarli";
+  } else {
+    elText.textContent = "Bilet olishga pulingiz yetarli emas ekan";
+  }
+}
